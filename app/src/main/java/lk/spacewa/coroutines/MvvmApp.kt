db5f6a66d0256b2c1f141c2384840fd71fb73d5e
Imdad <imdad482@gmail.com>
2020-08-13 18:29:39 +0530

    Setup work manager to run daily

--- app/src/main/java/lk/spacewa/coroutines/MvvmApp.kt
+++ app/src/main/java/lk/spacewa/coroutines/MvvmApp.kt
@@ -1,8 +1,16 @@
 package lk.spacewa.coroutines
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.*
 import dagger.hilt.android.HiltAndroidApp
+import lk.spacewa.coroutines.data.RefreshPokemonDataWork
 import lk.spacewa.coroutines.utils.AppLogger
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+
+
 
 /**
  * Created by Imdad on 05/11/20.
@@ -10,9 +18,40 @@ import lk.spacewa.coroutines.utils.AppLogger
 @HiltAndroidApp
 class MvvmApp : Application() {
 
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         AppLogger.init()
+        setupWorkManagerJob()
     }
 
+    /**
+     * Setup WorkManager background job to 'fetch' new pokemon data daily.
+     */
+    private fun setupWorkManagerJob() {
+        // initialize WorkManager with a Factory
+        val workManagerConfiguration = Configuration.Builder()
+                .setWorkerFactory(workerFactory)
+                .build()
+        WorkManager.initialize(this, workManagerConfiguration)
+
+        // Use constraints to require the work only run when the device is charging and the
+        // network is unmetered
+        val constraints = Constraints.Builder()
+                .setRequiresCharging(false)
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .build()
+
+        // Specify that the work should attempt to run every day
+        val work = PeriodicWorkRequestBuilder<RefreshPokemonDataWork>(1, TimeUnit.DAYS)
+                .setConstraints(constraints)
+                .build()
+
+        // Enqueue it work WorkManager, keeping any previously scheduled jobs for the same
+        // work.
+        WorkManager.getInstance(this)
+                .enqueueUniquePeriodicWork(RefreshPokemonDataWork::class.java.name, ExistingPeriodicWorkPolicy.KEEP, work)
+    }
 }
--- app/src/main/java/lk/spacewa/coroutines/di/module/AppModule.kt
+++ app/src/main/java/lk/spacewa/coroutines/di/module/AppModule.kt
@@ -1,7 +1,12 @@
 package lk.spacewa.coroutines.di.module
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
@@ -11,6 +16,7 @@ import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import lk.spacewa.coroutines.data.AppDataManager
 import lk.spacewa.coroutines.data.DataManager
+import lk.spacewa.coroutines.data.RefreshPokemonDataWork
 import lk.spacewa.coroutines.data.local.db.AppDatabase
 import lk.spacewa.coroutines.data.local.db.PokemonDBRepo
 import lk.spacewa.coroutines.data.local.db.PokemonDBRepoImpl
@@ -23,6 +29,7 @@ import lk.spacewa.coroutines.di.PreferenceInfo
 import lk.spacewa.coroutines.utils.AppConstants
 import lk.spacewa.coroutines.utils.rx.AppSchedulerProvider
 import lk.spacewa.coroutines.utils.rx.SchedulerProvider
+import timber.log.Timber
 import javax.inject.Singleton
 
 /**
@@ -68,6 +75,17 @@ class AppModule {
     @Singleton
     fun provideAppDatabase(@DatabaseInfo dbName: String?,@ApplicationContext context: Context?): AppDatabase {
         return Room.databaseBuilder(context!!, AppDatabase::class.java, dbName!!).fallbackToDestructiveMigration()
+                .addCallback(object : RoomDatabase.Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+//   This block will run the first time the DB is created and
+//   Work manager can be run here with the following commented lines of code but since we are already running the work manager in the application class
+//   this is redundant
+
+//                        val request = OneTimeWorkRequestBuilder<RefreshPokemonDataWork>().build()
+//                        WorkManager.getInstance(context).enqueue(request)
+                    }
+                })
                 .build()
     }
 
